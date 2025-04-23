d9d255cd59a4246254ffdef19ebfcd9eca7cf35f
tyrell-snyders <tyrell.snyders@gmail.com>
2025-04-23 12:04:33 +0200

    Added memoization to the VolumeComponent

--- src/components/main/volumes/VolumeComponent.tsx
+++ src/components/main/volumes/VolumeComponent.tsx
@@ -1,18 +1,33 @@
 import { Volume } from "../../../types";
-import { MouseEventHandler } from "react";
+import React, { MouseEventHandler } from "react";
 
 interface Props {
     volume: Volume;
     onClick: MouseEventHandler<HTMLButtonElement>
 }
 
-export default function VolumeComponent({ volume, onClick }: Props) {
-    const progress = Math.round((volume.used_gb / Math.max(volume.total_gb, 1)) * 100);
+const compare = (prev: Volume, next: Volume) => {
     return (
-        <button onClick={onClick} className="p-5 m-10 w-56 bg-darker radius rounded cursor-pointer">
-            <h3>{volume.name} ({volume.mountpoint})</h3>
-            <progress max="100" value={Number.isNaN(progress) ? 0 : progress} /> <br />
-            {volume.available_gb.toFixed(1)} GB free of {volume.total_gb.toFixed(1)} GB
-        </button>
-    )
+        prev.name === next.name &&
+        prev.mountpoint === next.mountpoint &&
+        prev.available_gb === next.available_gb &&
+        prev.total_gb === next.total_gb &&
+        prev.used_gb === next.used_gb
+    );
 }
+
+
+const VolumeComponent = React.memo(({ volume, onClick }: Props) => {
+  const progress = Math.round((volume.used_gb / Math.max(volume.total_gb, 1)) * 100);
+  
+  return (
+    <button onClick={onClick} className="p-5 m-10 w-56 bg-darker radius rounded cursor-pointer">
+      <h3>{volume.name} ({volume.mountpoint})</h3>
+      <progress max="100" value={progress} />
+      <br />
+      {volume.available_gb.toFixed(1)} GB free of {volume.total_gb.toFixed(1)} GB
+    </button>
+  );
+}, (prev: Props, next: Props) => compare(prev.volume, next.volume));
+
+export default VolumeComponent;
